116b2b085d70aa0d1efc5f22e092bd521237c31d
Iqbolshoh <iilhomjonov777@gmail.com>
2024-11-24 06:39:02 +0500

    commit

--- includes/js.php
+++ includes/js.php
@@ -1,12 +1,12 @@
   <!-- jQuery -->
-  <script src="includes/js/jquery.min.js"></script>
+  <script src="src/js/jquery.min.js"></script>
 
   <!-- Bootstrap 4 -->
-  <script src="includes/js/bootstrap.bundle.min.js"></script>
+  <script src="src/js/bootstrap.bundle.min.js"></script>
 
   <!-- SweetAlert2 -->
-  <script src="includes/js/sweetalert2.min.js"></script>
+  <script src="src/js/sweetalert2.min.js"></script>
 
   <!-- Toastr -->
-  <script src="includes/js/toastr.min.js"></script>
-  <script src="includes/js/adminlte.min.js"></script>
+  <script src="src/js/toastr.min.js"></script>
+  <script src="src/js/adminlte.min.js"></script>
