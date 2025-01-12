7322a8d25b4147300b3dc0c43ff272169a009dc6
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-12 21:32:20 +0500

    commit

--- index.php
+++ index.php
@@ -117,12 +117,6 @@
 
   <!-- Bootstrap 4 -->
   <script src="./src/js/bootstrap.bundle.min.js"></script>
-
-  <!-- SweetAlert2 -->
-  <script src="./src/js/sweetalert2.min.js"></script>
-
-  <!-- Toastr -->
-  <script src="./src/js/toastr.min.js"></script>
   <script src="./src/js/adminlte.min.js"></script>
 </body>
 
