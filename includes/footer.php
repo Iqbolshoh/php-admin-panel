a1548be6b183a8f6b81f9bd4ad3a957f50e3d612
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-12 21:48:52 +0500

    commit

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-inline">
         Admin Panel
     </div>
-    <strong>Copyright &copy; 2025 <a href="https://Iqbolshoh.uz">Iqbolshoh.uz</a>.</strong> All rights reserved.
+    <strong>Iqbolshoh &copy;2025 <a href="https://Iqbolshoh.uz">Iqbolshoh.uz</a>.</strong> All rights reserved.
 </footer>
