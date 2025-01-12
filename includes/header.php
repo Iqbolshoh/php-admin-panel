c0491d280f16626a699953b8b3ea27abbc306f81
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-12 21:39:27 +0500

    commit

--- includes/header.php
+++ includes/header.php
@@ -33,7 +33,7 @@ function renderHeader($pageTitle, $breadcrumbItems)
     <!-- Left side of the navbar -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="./" class="nav-link">Home</a>
