efc30bdbd3950fbb4363dfc67c508bae975f0854
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-12 21:27:05 +0500

    commit

--- alerts.php
+++ alerts.php
@@ -6,7 +6,23 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Admin Panel | Alerts</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php include 'includes/css.php' ?>
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="./favicon.ico">
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./src/css/adminlte.min.css">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="./src/css/sweetalert2-theme-bootstrap-4.css">
+
+  <!-- Toastr -->
+  <link rel="stylesheet" href="./src/css/toastr.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
