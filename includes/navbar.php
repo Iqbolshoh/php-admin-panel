144539d4e5b82852d6c7682425cc3d277fb6d636
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-12 20:08:50 +0500

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -38,9 +38,6 @@ function pagePath($pageTitle, $breadcrumb)
         <li class="nav-item d-none d-sm-inline-block">
             <a href="./" class="nav-link">Home</a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#logout" class="nav-link">Logout</a>
-        </li>
     </ul>
 
     <!-- SEARCH FORM -->
