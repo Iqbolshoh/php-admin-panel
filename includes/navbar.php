e88dde86f2b63984f6dac424ffd866d66f1b1b80
iqbolshoh <iilhomjonov777@gmail.com>
2025-01-12 20:50:48 +0500

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -75,7 +75,7 @@ function pagePath($pageTitle, $breadcrumb)
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="./" class="brand-link">
-        <img src="./src/images/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="./src/images/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -85,7 +85,7 @@ function pagePath($pageTitle, $breadcrumb)
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="./src/images/iqbolshoh.svg" class="img-circle elevation-2" alt="User Image">
+                <img src="./src/images/default.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="./" class="d-block">Iqbolshoh Ilhomjonov</a>
