32d809057098888d7e47fdc27bb3699b16ef1e73
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-05 12:11:17 +0500

    commit

--- includes/header.php
+++ includes/header.php
@@ -133,3 +133,20 @@ foreach ($menuItems as $menuItem) {
         </nav>
     </div>
 </aside>
+<script>
+    function logout() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You will be logged out!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, log me out!'
+        }).then((result) => {
+            if (result.value) {
+                window.location.href = './logout/';
+            }
+        });
+    }
+</script>
