b25111761830645d326adb87afbe1ab1adc4e48e
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-07 11:43:06 +0500

    commit

--- alerts.php
+++ alerts.php
@@ -5,12 +5,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Admin Panel | Alerts</title>
+  <link rel="icon" type="image/x-icon" href="./favicon.ico">
   <link rel="stylesheet" href="./src/css/adminlte.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-  <link rel="stylesheet" href="./src/css/sweetalert2-theme-bootstrap-4.css">
-  <link rel="stylesheet" href="./src/css/toastr.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
